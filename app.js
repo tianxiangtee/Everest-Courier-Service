c45b5e916de3a0728a5433a8a129e5d7c47a5ecd
tianxiangtee <47767907+tianxiangtee@users.noreply.github.com>
2023-03-06 05:38:44 +0000

    Add some basic  error handling for input

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const DeliveryService = require('./delivery/delivery-service');
 
 const args = process.argv.slice(2);
+
 const baseDeliveryCost = parseFloat(args.shift());
+
+if (isNaN(baseDeliveryCost)) {
+  console.error('Invalid base delivery cost');
+  process.exit(1);
+}
+
 const numberOfPackages = parseInt(args.shift());
+
+if (isNaN(numberOfPackages)) {
+  console.error('Invalid number of packages');
+  process.exit(1);
+}
 
 const packages = [];
 
@@ -11,6 +23,11 @@ for (let i = 0; i < numberOfPackages; i++) {
   const pkgWeight = parseFloat(args.shift());
   const distance = parseFloat(args.shift());
   const offerCode = args.shift();
+
+  if (!pkgId || isNaN(pkgWeight) || isNaN(distance)) {
+    console.error('Invalid package parameters');
+    process.exit(1);
+  }
 
   packages.push({
     id: pkgId,
@@ -21,8 +38,25 @@ for (let i = 0; i < numberOfPackages; i++) {
 }
 
 const noOfVehicles = parseInt(args.shift());
+
+if (isNaN(noOfVehicles)) {
+  console.error('Invalid number of vehicles');
+  process.exit(1);
+}
+
 const maxSpeed = parseInt(args.shift());
+
+if (isNaN(maxSpeed)) {
+  console.error('Invalid maximum speed');
+  process.exit(1);
+}
+
 const maxCarriableWeight = parseInt(args.shift());
+
+if (isNaN(maxCarriableWeight)) {
+  console.error('Invalid maximum carriable weight');
+  process.exit(1);
+}
 
 const deliveryService = new DeliveryService(
   baseDeliveryCost,
